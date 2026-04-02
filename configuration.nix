b3d9ee1c76d968f240b7056e720ccb780e75aaeb
ddavejjackson <protoblaster11@outlook.ie>
2026-04-02 19:53:58 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
 
 {
-
   ##########################################
-  # Import Hardware Configuration          #
+  # Imports
   ##########################################
   imports = [
     ./hardware-configuration.nix
   ];
 
   ##########################################
-  # Garbage Collection                     #
+  # System Version
+  ##########################################
+  system.stateVersion = "25.11";
+
+  ##########################################
+  # Nix Settings
   ##########################################
   nix.gc = {
     automatic = true;
@@ -18,75 +22,81 @@
     options = "--delete-older-than 7d";
   };
 
-  ##########################################
-  # ZRAM                                   #
-  ##########################################
-  zramSwap = {
-    enable = true;
-    memoryPercent = 50;
-    algorithm = "zstd";
-  };
-
-  ##########################################
-  # Allow Unfree Packages                  #
-  ##########################################
   nixpkgs.config.allowUnfree = true;
 
   ##########################################
-  # Boot loader                            #
+  # Bootloader (UEFI + GRUB)
   ##########################################
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    device = "nodev";
-  }; 
-
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+    };
+    efi.canTouchEfiVariables = true;
+  };
 
   ##########################################
-  # Locale (Ireland )                      #
+  # Kernel / Intel GPU tweaks
+  ##########################################
+  boot.kernelParams = [
+    "i915.enable_psr=2"
+    "i915.enable_fbc=1"
+    "i915.fastboot=1"
+  ];
+
+  ##########################################
+  # Locale & Time
   ##########################################
   time.timeZone = "Europe/Dublin";
- 
-  i18n.defaultLocale = "en_IE.UTF-8";
-  
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
+
+  i18n = {
+    defaultLocale = "en_IE.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_IE.UTF-8";
+      LC_IDENTIFICATION = "en_IE.UTF-8";
+      LC_MEASUREMENT = "en_IE.UTF-8";
+      LC_MONETARY = "en_IE.UTF-8";
+      LC_NAME = "en_IE.UTF-8";
+      LC_NUMERIC = "en_IE.UTF-8";
+      LC_PAPER = "en_IE.UTF-8";
+      LC_TELEPHONE = "en_IE.UTF-8";
+      LC_TIME = "en_IE.UTF-8";
+    };
   };
 
   console.keyMap = "ie";
 
-  services.xserver.xkb = {
-    layout = "ie";
-    variant = "";
-  };
-
   ##########################################
-  # Networking                             #
-  ##########################################
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  ##########################################
-  # Desktop (Cinnamon)                     #
+  # X11 / Keyboard
   ##########################################
   services.xserver = {
     enable = true;
-
-    desktopManager.cinnamon.enable = true;
-    displayManager.lightdm.enable = true;
+    xkb.layout = "ie";
   };
 
   ##########################################
-  # Audio (Modern)                         #
+  # KDE Plasma 6 (Wayland)
+  ##########################################
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  services.desktopManager.plasma6.enable = true;
+
+  ##########################################
+  # Networking
+  ##########################################
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    networkmanager.wifi.powersave = false;
+    firewall.enable = true;
+  };
+
+  ##########################################
+  # Audio (PipeWire)
   ##########################################
   services.pipewire = {
     enable = true;
@@ -97,25 +107,45 @@
   };
 
   ##########################################
-  # Intel CPU / microcode                  #
+  # Hardware (Modern 25.11 way)
   ##########################################
-  hardware.cpu.intel.updateMicrocode = true;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+
+      extraPackages = with pkgs; [
+        intel-media-driver
+        intel-vaapi-driver
+        libvdpau-va-gl
+      ];
+    };
+  };
 
   ##########################################
-  # Intel GPU (UHD 620 tweaks)             #
-  ##########################################
-  boot.kernelParams = [
-    "i915.enable_psr=1"
-    "i915.enable_fbc=1"
-  ];
-  
-  ##########################################
-  # Power (Simple Modern Approach          #
+  # Power Management
   ##########################################
   services.power-profiles-daemon.enable = true;
+  services.thermald.enable = true;
 
   ##########################################
-  # User                                   #
+  # ZRAM (Better memory usage)
+  ##########################################
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;
+    algorithm = "zstd";
+  };
+
+  ##########################################
+  # Time Sync
+  ##########################################
+  services.timesyncd.enable = true;
+
+  ##########################################
+  # User
   ##########################################
   users.users.cyb3r = {
     isNormalUser = true;
@@ -126,49 +156,86 @@
   };
 
   ##########################################
-  # Packages                               #
+  # Fonts
+  ##########################################
+  fonts = {
+    packages = with pkgs; [
+      dejavu_fonts
+      liberation_ttf
+      noto-fonts
+      noto-fonts-color-emoji
+      jetbrains-mono
+    ];
+
+    fontconfig.defaultFonts = {
+      sansSerif = [ "Noto Sans" ];
+      monospace = [ "JetBrains Mono" ];
+    };
+  };
+
+  ##########################################
+  # System Packages
   ##########################################
   environment.systemPackages = with pkgs; [
-    firefox
+    # Editors / Dev
     vim
     nano
     vscode
+    gcc
+    gdb
+    python3
+    nodejs
+
+    # CLI tools
     htop
     curl
     wget
-    python3
-    gcc
-    gdb
-    xdg-user-dirs
+
+    # Desktop apps
+    firefox
+    alacritty
     obs-studio
+
+    # KDE apps
+    kdePackages.kate
+    kdePackages.konsole
+    kdePackages.dolphin
+    kdePackages.kdeconnect-kde
+
+    # System tools
+    xdg-user-dirs
+    efibootmgr
+
+    # Docs
     man-db
     man-pages
     texinfo
-    efibootmgr
-    alacritty
+
+    # Office Related
+    libreoffice-fresh
+    obsidian
+
+    # Virtual Machines
+    open-vm-tools
+    vmware-workstation
+    linuxKernel.packages.linux_6_12.vmware
+
+    # VPN
+    openvpn
+
+    # Theming (Windows 11 for familiarity)
+    whitesur-kde
+    whitesur-icon-theme
+    whitesur-cursors
+    kdePackages.kde-gtk-config
+
+    unzip
   ];
 
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    liberation_ttf
-    noto-fonts
-    noto-fonts-color-emoji
-  ];
-
   ##########################################
-  # Time Sync                              #
+  # Optional Realtek Fix (if needed)
   ##########################################
-  services.timesyncd.enable = true;
-
-  ##########################################
-  # Firewall                               #
-  ##########################################
-  networking.firewall.enable = true;
-  
-  ##########################################
-  # System Version                         #
-  ##########################################
-  system.stateVersion = "25.11";
-
+  # boot.extraModprobeConfig = ''
+  #   options rtw88_pci disable_aspm=1
+  # '';
 }
-
