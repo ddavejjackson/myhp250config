08dcdbcb96a7310fea01429fd824d609a94a4577
ddavejjackson <protoblaster11@outlook.ie>
2026-04-15 18:47:38 +0100

    Update configuration.nix - reduce file size and switch to cinnamon

--- configuration.nix
+++ configuration.nix
@@ -1,237 +1,103 @@
 { config, pkgs, ... }:
 
 {
-  ##########################################
-  # Imports
-  ##########################################
   imports = [
     ./hardware-configuration.nix
   ];
 
-  ##########################################
-  # System Version
-  ##########################################
   system.stateVersion = "25.11";
 
-  ##########################################
-  # Nix Settings
-  ##########################################
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname
+  networking.hostName = "hp250g7";
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Time zone and locale
+  time.timeZone = "Europe/Dublin";
+  i18n.defaultLocale = "en_IE.UTF-8";
+
+  # Keyboard layout
+  services.xserver.xkb = {
+    layout = "ie";
+    variant = "";
   };
 
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  ##########################################
-  # Bootloader (UEFI + GRUB)
-  ##########################################
-  boot.loader = {
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-    };
-    efi.canTouchEfiVariables = true;
-  };
+  # Graphics
+  hardware.graphics.enable = true;
 
-  ##########################################
-  # Kernel / Intel GPU tweaks
-  ##########################################
-  boot.kernelParams = [
-    "i915.enable_psr=2"
-    "i915.enable_fbc=1"
-    "i915.fastboot=1"
-  ];
+  # Desktop environment
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
-  ##########################################
-  # Locale & Time
-  ##########################################
-  time.timeZone = "Europe/Dublin";
-
-  i18n = {
-    defaultLocale = "en_IE.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_IE.UTF-8";
-      LC_IDENTIFICATION = "en_IE.UTF-8";
-      LC_MEASUREMENT = "en_IE.UTF-8";
-      LC_MONETARY = "en_IE.UTF-8";
-      LC_NAME = "en_IE.UTF-8";
-      LC_NUMERIC = "en_IE.UTF-8";
-      LC_PAPER = "en_IE.UTF-8";
-      LC_TELEPHONE = "en_IE.UTF-8";
-      LC_TIME = "en_IE.UTF-8";
-    };
-  };
-
-  console.keyMap = "ie";
-
-  ##########################################
-  # X11 / Keyboard
-  ##########################################
-  services.xserver = {
-    enable = true;
-    xkb.layout = "ie";
-  };
-
-  ##########################################
-  # KDE Plasma 6 (Wayland)
-  ##########################################
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-
-  services.desktopManager.plasma6.enable = true;
-
-  ##########################################
-  # Networking
-  ##########################################
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-    networkmanager.wifi.powersave = false;
-    firewall.enable = true;
-  };
-
-  ##########################################
-  # Audio (PipeWire)
-  ##########################################
+  # Audio
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
   };
 
-  ##########################################
-  # Hardware (Modern 25.11 way)
-  ##########################################
-  hardware = {
-    cpu.intel.updateMicrocode = true;
+  # Power management
+  services.power-profiles-daemon.enable = true;
 
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-
-      extraPackages = with pkgs; [
-        intel-media-driver
-        intel-vaapi-driver
-        libvdpau-va-gl
-      ];
+  # Git
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      # Set these to your actual details
+      user.name = "Your Name";
+      user.email = "you@example.com";
     };
   };
 
-  ##########################################
-  # Power Management
-  ##########################################
-  services.power-profiles-daemon.enable = true;
-  services.thermald.enable = true;
+  # Docker
+  virtualisation.docker.enable = true;
 
-  ##########################################
-  # ZRAM (Better memory usage)
-  ##########################################
-  zramSwap = {
+  # VirtualBox host
+  virtualisation.virtualbox.host = {
     enable = true;
-    memoryPercent = 50;
-    algorithm = "zstd";
+    enableExtensionPack = true;
   };
 
-  ##########################################
-  # Time Sync
-  ##########################################
-  services.timesyncd.enable = true;
-
-  ##########################################
-  # User
-  ##########################################
+  # User account
   users.users.cyb3r = {
     isNormalUser = true;
+    description = "Cyb3r";
     extraGroups = [
       "wheel"
       "networkmanager"
+      "docker"
+      "vboxusers"
     ];
   };
 
-  ##########################################
-  # Fonts
-  ##########################################
-  fonts = {
-    packages = with pkgs; [
-      dejavu_fonts
-      liberation_ttf
-      noto-fonts
-      noto-fonts-color-emoji
-      jetbrains-mono
-    ];
-
-    fontconfig.defaultFonts = {
-      sansSerif = [ "Noto Sans" ];
-      monospace = [ "JetBrains Mono" ];
-    };
-  };
-
-  ##########################################
-  # System Packages
-  ##########################################
+  # Packages
   environment.systemPackages = with pkgs; [
-    # Editors / Dev
-    vim
-    nano
-    vscode
-    gcc
-    gdb
-    python3
-    nodejs
-
-    # CLI tools
-    htop
-    curl
     wget
-
-    # Desktop apps
-    firefox
-    alacritty
-    obs-studio
-
-    # KDE apps
-    kdePackages.kate
-    kdePackages.konsole
-    kdePackages.dolphin
-    kdePackages.kdeconnect-kde
-
-    # System tools
-    xdg-user-dirs
-    efibootmgr
-
-    # Docs
-    man-db
-    man-pages
-    texinfo
-
-    # Office Related
-    libreoffice-fresh
-    obsidian
-
-    # VPN
-    openvpn
-
-    # Theming (Windows 11 for familiarity)
-    kdePackages.kde-gtk-config
-    candy-icons
-    sweet
-    sweet-nova
-
+    curl
+    vim
+    htop
     unzip
-    tar
+
+    google-chrome
+    vscode
+    virtualbox
   ];
 
-  ##########################################
-  # Optional Realtek Fix (if needed)
-  ##########################################
-  # boot.extraModprobeConfig = ''
-  #   options rtw88_pci disable_aspm=1
-  # '';
+  # Enable flakes and modern nix commands
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Firewall
+  networking.firewall.enable = true;
+
+  # Sudo
+  security.sudo.enable = true;
 }
